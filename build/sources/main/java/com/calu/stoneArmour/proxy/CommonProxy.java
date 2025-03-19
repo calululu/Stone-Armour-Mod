b4a4fb9148669a54d5efcf0fe32e04a8815980c7
calululu <145708702+calululu@users.noreply.github.com>
2025-03-19 11:56:57 +0100

    Delete build directory

--- build/sources/main/java/com/calu/stoneArmour/proxy/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-package com.calu.stoneArmour.proxy;
-
-import net.minecraft.item.Item;
-
-public class CommonProxy {
-  public void registerItemRenderer(Item item, int meta, String id) {}
-}
--- build/sources/main/java/com/calu/stoneArmour/util/IHasModel.java
+++
@@ -1,6 +0,0 @@
-package com.calu.stoneArmour.util;
-
-public interface IHasModel {
-
-	public void registerModels();
-}
